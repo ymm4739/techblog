5d87631163e2beea9a7041802f03fc2302b52d22
youfan <you.fan@jfsys.com>
2019-11-07 17:21:42 +0800

    init client

---
+++ client/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Home {{name}}</h1>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      name: 'Youfan'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
